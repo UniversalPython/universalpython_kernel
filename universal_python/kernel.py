1c543a287f3518212499fcfd148cca564d098b28
Saad A. Bazaz <saadbazaz@hotmail.com>
2025-07-20 11:45:48 +0200

    update kernel to latest universalpython spec

--- universal_python/kernel.py
+++ universal_python/kernel.py
@@ -2,21 +2,12 @@ from ipykernel.kernelbase import Kernel
 
 from ipykernel.ipkernel import IPythonKernel
 
-from universalpython import run_module, SCRIPTDIR
+from universalpython import run_module
+
+import traceback
 
 from io import StringIO
 import sys
-
-import os
-
-LANG_DICTIONARIES = {
-    "ur": os.path.join(SCRIPTDIR, 'languages/ur/ur_native.lang.yaml'),
-    "hi": os.path.join(SCRIPTDIR, 'languages/hi/hi_native.lang.yaml'),
-    "cs": os.path.join(SCRIPTDIR, 'languages/cs/cs_native.lang.yaml'),
-    "de": os.path.join(SCRIPTDIR, 'languages/de/de_native.lang.yaml'),
-    "en": os.path.join(SCRIPTDIR, 'languages/en/en_native.lang.yaml'),
-}
-
 class UniversalPythonKernel(IPythonKernel):
     implementation = 'UniversalPython'
     implementation_version = '1.1'
@@ -31,38 +22,41 @@ class UniversalPythonKernel(IPythonKernel):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.current_language = "ur"  # default language
+        self.current_language = ""
 
     def get_language_and_code(self, code):
-        lines = code.splitlines()
-        lang = self.current_language
-        if lines and lines[0].strip().startswith("# language:"):
-            lang_flag = lines[0].strip().split(":", 1)[-1].strip()
-            if lang_flag in LANG_DICTIONARIES:
-                lang = lang_flag
-                self.current_language = lang  # persist for future cells
-            code = "\n".join(lines[1:])  # Remove the flag line
-        return lang, code
+            lines = code.splitlines()
+            lang = self.current_language
+            if lines and lines[0].strip().startswith("# language:"):
+                lang_flag = lines[0].strip().split(":", 1)[-1].strip()
+                if lang_flag:
+                    lang = lang_flag
+                    self.current_language = lang  # persist for future cells
+                    code = "\n".join(lines[1:])  # Remove the flag line
+            return lang
 
     def do_execute(self, code, silent, store_history=True, user_expressions=None,
                    allow_stdin=False):
         error_thrown = False
 
-        lang, code = self.get_language_and_code(code)
-        dictionary_path = LANG_DICTIONARIES.get(lang, LANG_DICTIONARIES["ur"])
+        compiled_code = ""
+        lang = self.get_language_and_code(code)
 
         try:
             compiled_code = run_module("lex", code, args = {
                             'translate': True,
-                            'dictionary': dictionary_path,
                             'reverse': False,
+                            'dictionary': '',
+                            'source_language': lang,
                             'keep': False,         
                             'keep_only': False,
                             'return': True,
+                            'file': '',
+                            'suppress_warnings': True,  # Suppress warnings during compilation
                         })
         except Exception as e:
             error_thrown = True
-            error_message = "Error: " + str(e)
+            error_message = "Error while compiling code: " + str(e)
             print (error_message)
 
         return super(UniversalPythonKernel, self).do_execute(compiled_code, silent, store_history, user_expressions,
@@ -72,16 +66,18 @@ class UniversalPythonKernel(IPythonKernel):
     def do_complete(self, code, cursor_pos):
         error_thrown = False
 
-        lang, code = self.get_language_and_code(code)
-        dictionary_path = LANG_DICTIONARIES.get(lang, LANG_DICTIONARIES["ur"])
+        lang = self.get_language_and_code(code)
 
         compiled_code = run_module("lex", code, args = {
             'translate': True,
-            'dictionary': dictionary_path,
             'reverse': False,
+            'dictionary': '',
+            'source_language': lang,
             'keep': False,         
             'keep_only': False,
             'return': True,
+            'file': '',
+            'suppress_warnings': True,  # Suppress warnings during compilation
         })
 
         return super(UniversalPythonKernel, self).do_complete(compiled_code, cursor_pos)
